91e52e87f358d33d34493c78c98f8aeac2e4152c
sadikweb <159684791+sadikweb@users.noreply.github.com>
2024-02-16 00:39:30 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.type', {
+    strings: [
+        '"Car-Booking"',
+        '"Car-Service"',
+        ' "service- In-Your-Area"'
+    ],
+    typeSpeed: 150,
+    loop: true,
+
+});
